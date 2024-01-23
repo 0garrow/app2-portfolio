f53a6415210cc540db650cd51eca42a2e4259d47
0garrow <0garrow@protonmail.com>
2024-01-23 02:07:28 +0000

    Add description on the new columns

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ with col2:
     st.title("Dan Toms")
     content = """
     About me bit
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin malesuada dapibus. Aenean vitae tortor bibendum elit accumsan sagittis. Nulla ultricies in ante non consequat. Suspendisse sed dolor et enim iaculis pulvinar non sed elit. Cras malesuada hendrerit eros. Nunc luctus auctor auctor. Curabitur volutpat, magna vel mollis imperdiet, quam massa molestie turpis, quis vestibulum ligula quam eu arcu. Aliquam porttitor cursus ultrices. Proin feugiat massa eget augue faucibus, in consectetur neque facilisis. Sed tellus leo, blandit ut neque eu, semper congue metus. Cras in erat suscipit est pretium rutrum. Phasellus maximus placerat justo. Sed laoreet facilisis nulla, malesuada tincidunt sem fermentum vitae. Nunc venenatis eget justo sed imperdiet. 
     """
     st.info(content)
+
+content2 = """Below you can find some if the apps I have built in Python. Feel free to contact me."""
+
+st.write(content2)
