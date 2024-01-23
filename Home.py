45fd53d4b794713316f50650fade6b11fd177a6d
0garrow <0garrow@protonmail.com>
2024-01-23 12:57:29 +0000

    Add navigation menu and contact us page

--- Home.py
+++ Home.py
@@ -6,14 +6,12 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=350)
+    st.image("images/photo.png")
 
 with col2:
     st.title("Dan Toms")
     content = """
-    About me bit
-
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin malesuada dapibus. Aenean vitae tortor bibendum elit accumsan sagittis. Nulla ultricies in ante non consequat. Suspendisse sed dolor et enim iaculis pulvinar non sed elit. Cras malesuada hendrerit eros. Nunc luctus auctor auctor. Curabitur volutpat, magna vel mollis imperdiet, quam massa molestie turpis, quis vestibulum ligula quam eu arcu. Aliquam porttitor cursus ultrices. Proin feugiat massa eget augue faucibus, in consectetur neque facilisis. Sed tellus leo, blandit ut neque eu, semper congue metus. Cras in erat suscipit est pretium rutrum. Phasellus maximus placerat justo. Sed laoreet facilisis nulla, malesuada tincidunt sem fermentum vitae. Nunc venenatis eget justo sed imperdiet. 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin malesuada dapibus. Aenean vitae tortor bibendum elit accumsan sagittis. Nulla ultricies in ante non consequat. Suspendisse sed dolor et enim iaculis pulvinar non sed elit. 
     """
     st.info(content)
 
